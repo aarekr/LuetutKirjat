168a7cf0c33d4504fa61fd85aca58ca008a3727f
Aare <aare212@hotmail.com>
2024-08-07 20:18:44 +0300

    Kirjautumisen runko

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 from os import getenv
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
@@ -13,6 +15,19 @@ def index():
     result = db.session.execute(text("SELECT * FROM lkbooks"))
     db_books = result.fetchall()
     return render_template("index.html", db_books=db_books)
+
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    # TODO: check username and password
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
 
 # form for adding a new book to the reading list
 @app.route("/addbook")
